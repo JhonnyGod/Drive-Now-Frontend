6596385a2fd51f7c8edcf075b55bf24184c828ac
Jhonny <jhony.duque@correounivalle.edu.co>
2024-10-30 22:34:39 -0500

    Adelanto de recuperación de contraseña

--- drive-now/src/pages/Login/login.jsx
+++ drive-now/src/pages/Login/login.jsx
@@ -48,7 +48,7 @@ export const Login = () => {
         localStorage.setItem('userData', JSON.stringify(userData)); //*Guardo la información del usuario en el LocalStorage
 
         navigate('/home'); //*Navegar a la página de inicio
-        
+
       }
     } catch (error) {
       console.error('Error al iniciar sesión:', error);
@@ -57,72 +57,72 @@ export const Login = () => {
     }
   };
 
-  const handleForgotPasswordClick = (e) => {
+  const forgotPasswordRecover = async (e) => {
+
     e.preventDefault();
     navigate('/recover');
   }
-
-  const showAlertWithAnimation = () => {
-    setShowAlert(true);
-    setAlertClass('alert-fall');
-
-    setTimeout(() => {
-      setAlertClass('alert-rise');
-
+    const showAlertWithAnimation = () => {
+      setShowAlert(true);
+      setAlertClass('alert-fall');
 
       setTimeout(() => {
-        setShowAlert(false);
-        setAlertClass('');
-      }, 500);
-    }, 3000);
-  };
+        setAlertClass('alert-rise');
 
-  return (
-    <div className="container">
-      <form onSubmit={handleSubmit} className="form">
-        <div className="profile-image-container">
-          <img src={profileImage} alt="Perfil" className="profile-image" />
-        </div>
-        <h2 className="title">Iniciar Sesión</h2>
-        <div className="input-group">
-          <label htmlFor="email" className="label">Email</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            className="input"
-            placeholder="Ingresa tu correo"
-          />
-        </div>
-        <div className="input-group">
-          <label htmlFor="password" className="label">Contraseña</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            className="input"
-            placeholder="Ingresa tu contraseña"
-          />
-        </div>
-        <button type="submit" className="button">Iniciar</button>
-        <div className="link">
-          <div className="forgot-password">
-            <a href="#" className="forgot-password-link" onClick={handleForgotPasswordClick}>¿Olvidaste tu contraseña?</a>
+
+        setTimeout(() => {
+          setShowAlert(false);
+          setAlertClass('');
+        }, 500);
+      }, 3000);
+    };
+
+    return (
+      <div className="container">
+        <form onSubmit={handleSubmit} className="form">
+          <div className="profile-image-container">
+            <img src={profileImage} alt="Perfil" className="profile-image" />
           </div>
-          <div className="register">
-            <a href="#" className="register-link">¿No tienes una cuenta?</a>
+          <h2 className="title">Iniciar Sesión</h2>
+          <div className="input-group">
+            <label htmlFor="email" className="label">Email</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="input"
+              placeholder="Ingresa tu correo"
+            />
           </div>
-        </div>
-        {showAlert && (
-          <div className={`alert ${alertClass}`}>
-            <p>{alertMessage}</p>
+          <div className="input-group">
+            <label htmlFor="password" className="label">Contraseña</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="input"
+              placeholder="Ingresa tu contraseña"
+            />
           </div>
-        )}
-      </form>
-    </div>
-  );
-};
+          <button type="submit" className="button">Iniciar</button>
+          <div className="link">
+            <div className="forgot-password">
+              <a href="#" className="forgot-password-link" onClick={forgotPasswordRecover}>¿Olvidaste tu contraseña?</a>
+            </div>
+            <div className="register">
+              <a href="#" className="register-link">¿No tienes una cuenta?</a>
+            </div>
+          </div>
+          {showAlert && (
+            <div className={`alert ${alertClass}`}>
+              <p>{alertMessage}</p>
+            </div>
+          )}
+        </form>
+      </div>
+    );
+  };
--- drive-now/src/pages/PasswordForgot/recover.jsx
+++ drive-now/src/pages/PasswordForgot/recover.jsx
@@ -1,17 +1,61 @@
-import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 import './recover.css';
 
 const Recover = () => {
+    const [email, setEmail] = useState(''); 
+
+    //TODO: Le falta, pero en teoría ya debe estar enviando los correos de recuperación, queda pendiente la construcción de 
+    //* la página donde se escribe el codigo y cambiar la contraseña, pero me duele resto la cabeza, pitos
+
+    const recoverPasswordFunction = async (e) => {
+        e.preventDefault(); 
+
+        try {
+            const response = await axios.post('http://localhost:3000/usuario/recuperar', { email });
+            console.log("Solicitud enviada");
+
+            const data = response.data;
+            
+            const info = {
+                status: data.ok,
+                message: data.message
+            };
+
+            if (info.status === 200) {
+                console.log(info);
+            }
+
+            console.log(info);
+            setEmail('');
+
+            if (response.status !== 200) {
+                console.error('Error al recuperar contraseña:', response);
+                setEmail('');
+            } 
+
+        } catch (error) {
+            console.error('Error al recuperar contraseña:', error);
+            setEmail('');
+        }
+    };
 
     return (
         <div className="Recover">
             <h1>Recuperar contraseña</h1>
-            <form>
-                <input type="email" placeholder="Ingresa tu correo electrónico." />
-                <button>Enviar</button>
+            <form onSubmit={recoverPasswordFunction}>
+                <input
+                    type="email"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Ingresa tu correo electrónico."
+                    required
+                />
+                <button type="submit">Enviar</button>
             </form>
         </div>
     );
-}
+};
 
 export default Recover;
