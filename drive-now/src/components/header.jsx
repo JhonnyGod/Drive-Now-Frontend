59513a11360a7815ce0ce31d4b98832cf7d83d2a
Jhonny <jhony.duque@correounivalle.edu.co>
2024-11-03 14:30:38 -0500

    Homepage fixed

--- drive-now/src/components/header.jsx
+++ drive-now/src/components/header.jsx
@@ -5,45 +5,46 @@ import { useNavigate } from "react-router-dom";
 
 
 function Header() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleRegisterClick = () => {
-        navigate('/register'); // Navega a la ruta /register
-      };
+  const handleRegisterClick = () => {
+    navigate('/register'); // Navega a la ruta /register
+  };
 
-      const handleLogin = () => {
-        navigate('/Login'); // Navega a la ruta /login
-      }; 
+  const handleLogin = () => {
+    navigate('/Login'); // Navega a la ruta /login
+  };
 
-    return (
-      <header className="header">
-        <div className="logo">
-          <img src="logo.png" alt="Logo" /> {/* aqui va la ruta del logo*/ }
-          <h1>Drive Now</h1>
-        </div>
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="logo.png" alt="Logo" /> {/* aqui va la ruta del logo*/}
+        <h1>Drive Now</h1>
+      </div>
 
-        <div className="buttonsUser">
-            <button className="buttonLogin" onClick={handleLogin}>Login</button>
-            <button className="buttonSing" onClick={handleRegisterClick}>Sing up</button>
-        </div>
+      <input type="text" className="header-input" placeholder="Buscar..."></input>
 
-        <input type="text" className="header-input" placeholder="Buscar..."></input>
+      <nav className="nav">
+        <ul>
+          <li><a href="#home">Inicio</a></li>
+          <li><a href="#about">Sobre Nosotros</a></li>
+          <li><a href="#services">Servicios</a></li>
+          <li><a href="#contact">Contacto</a></li>
+        </ul>
+      </nav>
 
-        <nav className="nav">
-          <ul>
-            <li><a href="#home">Inicio</a></li>
-            <li><a href="#about">Sobre Nosotros</a></li>
-            <li><a href="#services">Servicios</a></li>
-            <li><a href="#contact">Contacto</a></li>
-          </ul>
-        </nav>
-      </header>
-    );
-    
-  }
-  export default Header;
- 
+      <div className="buttonsUser">
+        <button className="buttonLogin" onClick={handleLogin}>Login</button>
+        <button className="buttonSing" onClick={handleRegisterClick}>Sing up</button>
+      </div>
+      
+    </header>
+  );
+
+}
+export default Header;
 
 
 
-  
+
+
