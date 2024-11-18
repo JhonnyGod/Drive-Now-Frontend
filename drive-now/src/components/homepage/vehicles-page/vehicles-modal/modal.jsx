d98cc488062d7beae90eeb7376ef4d8c74382c55
Jhonny <jhony.duque@correounivalle.edu.co>
2024-11-17 23:15:35 -0500

    No se

--- drive-now/src/components/homepage/vehicles-page/vehicles-modal/modal.jsx
+++ drive-now/src/components/homepage/vehicles-page/vehicles-modal/modal.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './styles.css';
-
-
+import './VehiculoModal.css';
 
 export default function VehiculoModal({ vehiculo, onClose }) {
   const [isOpen, setIsOpen] = useState(false);
