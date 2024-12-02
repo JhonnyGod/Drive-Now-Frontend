038d2b2fd251543f008885af4a3da93d48feb9a2
Jhonny <jhony.duque@correounivalle.edu.co>
2024-12-01 22:12:43 -0500

    Recibiendo fechas de alquiler

--- drive-now/src/components/homepage/vehicles-page/vehicles-modal/DatePicker/Datepicker.jsx
+++ drive-now/src/components/homepage/vehicles-page/vehicles-modal/DatePicker/Datepicker.jsx
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 import "./StyledDatePicker.css";
 
-export default function StyledDatePicker() {
-  const [dateRange, setDateRange] = useState([null, null]);
-  const [startDate, endDate] = dateRange;
+export default function StyledDatePicker({ dateRange, setDateRange }) {
+  const [startDate, endDate] = dateRange;  // Usamos las fechas pasadas como props
 
   const currentDate = new Date();
   const currentYear = currentDate.getFullYear();
@@ -24,48 +23,37 @@ export default function StyledDatePicker() {
         startDate={startDate}
         endDate={endDate}
         onChange={(update) => {
-          setDateRange(update);
+          setDateRange(update);  // Actualiza el estado en el componente padre
         }}
-        minDate={new Date()}
+        minDate={new Date()}  // Asegúrate de que la fecha mínima sea hoy
         inline
         className="start-date-picker"
         calendarClassName="custom-calendar"
         dayClassName={() => "custom-day"}
         monthClassName={() => "custom-month"}
-        weekDayClassName={() => "custom-weekday"
-        }
-        renderCustomHeader={({
-          date,
-          changeYear,
-          changeMonth,
-          decreaseMonth,
-          increaseMonth,
-          prevMonthButtonDisabled,
-          nextMonthButtonDisabled,
-        }) => (
+        weekDayClassName={() => "custom-weekday"}
+        renderCustomHeader={(props) => (
           <div className="custom-header">
-            <button onClick={decreaseMonth} disabled={prevMonthButtonDisabled} className="navigation-button">
-              {"<"}
-            </button>
+            <button onClick={props.decreaseMonth} disabled={props.prevMonthButtonDisabled} className="navigation-button">{"<"}</button>
             <div className="month-year-selectors">
               <select
-                value={date.getMonth()}
-                onChange={({ target: { value } }) => changeMonth(Number(value))}
+                value={props.date.getMonth()}
+                onChange={({ target: { value } }) => props.changeMonth(Number(value))}
                 className="month-selector"
               >
                 {months.map((month, index) => (
                   <option 
                     key={month} 
                     value={index}
-                    disabled={date.getFullYear() === currentYear && index < currentMonth}
+                    disabled={props.date.getFullYear() === currentYear && index < currentMonth}
                   >
                     {month}
                   </option>
                 ))}
               </select>
               <select
-                value={date.getFullYear()}
-                onChange={({ target: { value } }) => changeYear(Number(value))}
+                value={props.date.getFullYear()}
+                onChange={({ target: { value } }) => props.changeYear(Number(value))}
                 className="year-selector"
               >
                 {years.map((year) => (
@@ -75,16 +63,14 @@ export default function StyledDatePicker() {
                 ))}
               </select>
             </div>
-            <button onClick={increaseMonth} disabled={nextMonthButtonDisabled} className="navigation-button">
-              {">"}
-            </button>
+            <button onClick={props.increaseMonth} disabled={props.nextMonthButtonDisabled} className="navigation-button">{">"}</button>
           </div>
         )}
       />
       {startDate && endDate && (
         <div className="date-range-display">
-          <p>Desde: {startDate.toLocaleDateString()}</p>
-          <p>Hasta: {endDate.toLocaleDateString()}</p>
+          <p className='desde-date'>Desde: {startDate.toLocaleDateString()}</p>
+          <p className='hasta-date'>Hasta: {endDate.toLocaleDateString()}</p>
         </div>
       )}
     </div>
--- drive-now/src/components/homepage/vehicles-page/vehicles-modal/modal.jsx
+++ drive-now/src/components/homepage/vehicles-page/vehicles-modal/modal.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import './styles.css';
 import useUserStore from '../../../../store/useUserStore';
-import axios from 'axios';
-import { Navigate, useNavigate } from 'react-router-dom';
-import StyledDatePicker from './DatePicker/Datepicker'
-
+import { useNavigate } from 'react-router-dom';
+import StyledDatePicker from './DatePicker/Datepicker';
 
 export default function VehiculoModal({ vehiculo, onClose }) {
     const [isOpen, setIsOpen] = useState(false);
     const { user, hasSession } = useUserStore();
     const [payment, setOpenPayment] = useState(false);
+    const [dateRange, setDateRange] = useState([null, null]);
+
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -18,11 +18,25 @@ export default function VehiculoModal({ vehiculo, onClose }) {
 
     const handleLogin = () => {
         navigate('/login');
-    }
+    };
+
     const handleRent = () => {
         setOpenPayment(true);
-    }
+    };
 
+    const handleRentPetition = async () => {
+        const [startDate, endDate] = dateRange;
+
+        if (startDate && endDate) {
+            const diffTime = Math.abs(endDate - startDate);
+            const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); // Calcular la diferencia en días
+            console.log(`Desde: ${startDate.toLocaleDateString()}`);
+            console.log(`Hasta: ${endDate.toLocaleDateString()}`);
+            console.log(`Cantidad de días: ${diffDays}`);
+        } else {
+            console.log("No se seleccionaron fechas válidas.");
+        }
+    };
 
     const handleClose = () => {
         setIsOpen(false);
@@ -33,53 +47,59 @@ export default function VehiculoModal({ vehiculo, onClose }) {
         <div className={`vm-modal-overlay ${isOpen ? 'vm-open' : ''}`}>
             <div className={`vm-modal-content ${isOpen ? 'vm-open' : ''}`}>
                 <button className="vm-modal-close" onClick={handleClose}>&times;</button>
-                {payment ? null : <div className="vm-modal-body">
-                    <div className="vm-vehicle-details">
-                        <div className="vm-vehicle-image-container">
-                            <img src={vehiculo.image_src} alt={vehiculo.nombre} className="vm-vehicle-image" />
-                        </div>
-                        <div className="vm-vehicle-info">
-                            <h2 className="vm-vehicle-title">{vehiculo.nombre}</h2>
-                            <p className="vm-vehicle-description">{vehiculo.marca} - {vehiculo.color}</p>
-                            <div className="vm-vehicle-features">
-                                <div className="vm-feature">
-                                    <span className="vm-feature-icon">🚗</span>
-                                    <span>Tipo: {vehiculo.tipovehiculo}</span>
-                                </div>
-                                <div className="vm-feature">
-                                    <span className="vm-feature-icon">🏷️</span>
-                                    <span>Marca: {vehiculo.marca}</span>
-                                </div>
-                                <div className="vm-feature">
-                                    <span className="vm-feature-icon">🎨</span>
-                                    <span>Color: {vehiculo.color}</span>
-                                </div>
-                                <div className="vm-feature">
-                                    <span className="vm-feature-icon">📅</span>
-                                    <span>Modelo: {vehiculo.modelo}</span>
-                                </div>
+                {payment ? null : (
+                    <div className="vm-modal-body">
+                        <div className="vm-vehicle-details">
+                            <div className="vm-vehicle-image-container">
+                                <img src={vehiculo.image_src} alt={vehiculo.nombre} className="vm-vehicle-image" />
                             </div>
-                            <div className="vm-vehicle-full-description">
-                                <h3>Descripción</h3>
-                                <p>{vehiculo.descripcion}</p>
+                            <div className="vm-vehicle-info">
+                                <h2 className="vm-vehicle-title">{vehiculo.nombre}</h2>
+                                <p className="vm-vehicle-description">{vehiculo.marca} - {vehiculo.color}</p>
+                                <div className="vm-vehicle-features">
+                                    <div className="vm-feature">
+                                        <span className="vm-feature-icon">🚗</span>
+                                        <span>Tipo: {vehiculo.tipovehiculo}</span>
+                                    </div>
+                                    <div className="vm-feature">
+                                        <span className="vm-feature-icon">🏷️</span>
+                                        <span>Marca: {vehiculo.marca}</span>
+                                    </div>
+                                    <div className="vm-feature">
+                                        <span className="vm-feature-icon">🎨</span>
+                                        <span>Color: {vehiculo.color}</span>
+                                    </div>
+                                    <div className="vm-feature">
+                                        <span className="vm-feature-icon">📅</span>
+                                        <span>Modelo: {vehiculo.modelo}</span>
+                                    </div>
+                                </div>
+                                <div className="vm-vehicle-full-description">
+                                    <h3>Descripción</h3>
+                                    <p>{vehiculo.descripcion}</p>
+                                </div>
+                                <button
+                                    className="vm-rent-button"
+                                    onClick={() => hasSession() ? handleRent() : handleLogin()}
+                                >
+                                    {hasSession() ? 'Alquilar' : 'Logearse'}
+                                </button>
                             </div>
-                            <button
-                                className="vm-rent-button"
-                                onClick={() => hasSession() ? handleRent() : handleLogin()}
-                            >
-                                {hasSession() ? 'Alquilar' : 'Logearse'}
-                            </button>
                         </div>
                     </div>
-                </div>}
-                {payment ?
+                )}
+                {payment ? (
                     <div className='payment-body'>
                         <div className='start-date-space'>
                             <h1 className='start-date-title'>Selecciona tu fecha de inicio y finalización de alquiler</h1>
-                            <StyledDatePicker />
-                            <button className='aceptar-button'>Aceptar</button>
+                            <StyledDatePicker
+                                dateRange={dateRange}    // Pasamos el estado
+                                setDateRange={setDateRange}  // Pasamos la función para actualizar el estado
+                            />
+                            <button className='aceptar-button' onClick={handleRentPetition}>Aceptar</button>
                         </div>
-                    </div> : null}
+                    </div>
+                ) : null}
             </div>
         </div>
     );
